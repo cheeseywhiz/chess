99555cfa8ed58b70f9b1f0aaa8a8dbb3ff76b714
Spencer Todd <cheesy.mac.dott@gmail.com>
2020-12-22 13:21:53 -0500

    move location of db file

--- bin/db.sh
+++ bin/db.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -Eeuxo pipefail
 
+DB=build/chess.sqlite3
+
 function assert_exists() {
-    if [[ ! -a chess.sqlite3 ]]; then
+    if [[ ! -a $DB ]]; then
         echo error: database does not exist
         return 1
     fi
 }
 
 function assert_not_exists() {
-    if [[ -a chess.sqlite3 ]]; then
+    if [[ -a $DB ]]; then
         echo error: database exists
         return 1
     fi
@@ -18,11 +20,11 @@ function assert_not_exists() {
 case $1 in
     create)
         assert_not_exists
-        sqlite3 chess.sqlite3 < sql/schema.sql
-        sqlite3 chess.sqlite3 < sql/data.sql
+        sqlite3 $DB < sql/schema.sql
+        sqlite3 $DB < sql/data.sql
         ;;
     destroy)
-        rm -f chess.sqlite3
+        rm -f $DB
         ;;
     reset)
         $0 destroy
@@ -30,10 +32,10 @@ case $1 in
         ;;
     dump)
         assert_exists
-        sqlite3 -batch -line chess.sqlite3 '.mode column' '.header on' 'SELECT * FROM users'
+        sqlite3 -batch -line $DB '.mode column' '.header on' 'SELECT * FROM users'
         ;;
     cmd)
-        sqlite3 chess.sqlite3
+        sqlite3 $DB
         ;;
     *)
         echo "Usage: $0 (create|destroy|reset|dump|cmd)"
