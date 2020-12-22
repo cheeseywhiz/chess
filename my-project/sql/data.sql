a7dfda891f84833bff3c4c668ecb0126887a67a4
Spencer Todd <cheesy.mac.dott@gmail.com>
2020-12-22 13:04:45 -0500

    remove hello world drogon project

--- my-project/sql/data.sql
+++
@@ -1,4 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-INSERT INTO users(username) VALUES
-('spencer');
--- my-project/sql/schema.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys = ON;
-
-CREATE TABLE users(
-    username VARCHAR NOT NULL,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (username)
-);
